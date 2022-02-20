3801c5a372e41b42a9c1601285276073cac150ad
psylopunk <psylopunk@protonmail.com>
2022-02-20 23:47:44 +0300

    v0.2

---
+++ README.md
@@ -0,0 +1,4 @@
+# ton
+Simple python client for The Open Network
+
+Documentation will be available here soon
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ton",
+    version="0.2",
+    author="psylopunk",
+    author_email="psylopunk@protonmail.com",
+    description="Python client for The Open Network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/psylopunk/ton",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'crc16==0.1.1',
+        'poetry==1.1.13',
+        'httpx==0.22.0',
+        'ujson==5.1.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
