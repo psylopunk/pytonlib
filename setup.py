ea90ce2ebc6f383b6e9b0b1da3e090e7a44825ea
psylopunk <psylopunk@protonmail.com>
2022-02-21 03:02:01 +0300

    v0.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/psylopunk/ton",
-    packages=['ton'],
+    packages=setuptools.find_packages(),
     install_requires=[
         'crc16==0.1.1',
         'poetry==1.1.13',
