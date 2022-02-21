2557a08d5db9905a153e9558e8cfe84d1c708b91
psylopunk <psylopunk@protonmail.com>
2022-02-21 05:14:32 +0300

    v0.8: Fix bugs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ton",
-    version="0.7",
+    version="0.8",
     author="psylopunk",
     author_email="psylopunk@protonmail.com",
     description="Python client for The Open Network",
