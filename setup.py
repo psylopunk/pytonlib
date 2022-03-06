87ff20fea9ea4c3b3c814e65fd1c0e3fe3e2b644
psylopunk <psylopunk@protonmail.com>
2022-03-06 17:10:12 +0300

    v0.11: Fix bugs and upgrade logger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ton",
-    version="0.10",
+    version="0.11",
     author="psylopunk",
     author_email="psylopunk@protonmail.com",
     description="Python client for The Open Network",
